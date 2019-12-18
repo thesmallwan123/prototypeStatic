9c7cd1f469ba98733de6f3a79d58fd810b468695
Post <ivar.post@klm.com>
2019-12-18 17:08:14 +0100

    remote lesson display: Known Bugs, Fix divider

--- prototype/src/app/body/my-path/component/component.component.ts
+++ prototype/src/app/body/my-path/component/component.component.ts
@@ -9,18 +9,23 @@ import { GetDataService } from '../../../services/get-data.service';
 export class ComponentComponent implements OnInit {
 
   components;
-  topics;
+  topics =[];
+  allLessons;
   length;
   result = [];
-  object = [];
-  id = 0;
+  allTopics;
+  lessons = [];
+  idTopic = 0;
+  idLesson = 0;
   classID: any;
   width;
   percentageDoneTopic;
 
+
   constructor(private getData: GetDataService) {}
   ngOnInit() {
     this.Comp();
+    // console.log(this.components[3].topic[1].lessons)
   }
   showLessons(id) {
     const element = document.getElementById("viewLesson" + id);
@@ -30,39 +35,52 @@ export class ComponentComponent implements OnInit {
     this.width = this.width - 32;
     setTimeout(function () {
       hideLesson(id);
-    }, 5000);
-
+    }, 100000);
+    
     function hideLesson(id) {
       const element = document.getElementById("viewLesson" + id);
       return element.classList.remove('viewLesson');
     }
   }
-
-
-
-
-
+  
+  
+  
+  
+  
   Comp() {
     this.components = this.getData.getComp();
     for (let i = 0; i < this.components.length; i++) {
       this.Top(i);
-      this.components[i].topic = this.object;
-      for (let b = 0; b < 1; b++) {
-        this.components[i].done = this.getData.calcPercentage(this.components[i].topic);
-      }
+      this.components[i].topic = this.topics;
+      this.components[i].done = this.getData.calcPercentage(this.components[i].topic);
     }
     return this.components;
   }
-
+  
   Top(i) {
-    this.topics = this.getData.getTopic();
-    this.object = [];
+    // console.log("Loopt door Component")
+    this.allTopics = this.getData.getTopic();
+    this.topics = [];
     for (let a = 0; a < this.components[i].topic.length; a++) {
-      this.object.push(this.topics[this.id]);
-      this.id = this.id + 1;
+      this.Lesson(a,i);
+      this.allTopics[this.idTopic].lessons = this.lessons;
+      // console.log(this.allTopics[this.idTopic].lessons)
+      this.topics.push(this.allTopics[this.idTopic]);
+      this.idTopic = this.idTopic + 1;
     }
   }
-
-
-
+  Lesson(a,i){
+    // console.log("Loopt door Topic")
+    this.allLessons = this.getData.getLessons();
+    this.lessons = [];
+    for(let c=0; c < this.allTopics[this.idTopic].lessons.length; c++){
+      this.idLesson = this.allTopics[this.idTopic].lessons[c];
+      this.lessons.push(this.allLessons[this.idLesson]);
+    }
+    // this.idLesson = this.topics[this.idTopic];
+    // for(let c =0; c<this.allTopics[this.idTopic].lessons.length; c++){
+    //   console.log(c)
+    //   // this.idLesson = this.idLesson +1;
+    // }
+  }
 }
